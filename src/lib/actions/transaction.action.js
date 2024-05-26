e22496edb29d73134030539e3146cdc4963188a3
Shantanu Dutta <shantanubhs1985@gmail.com>
2024-05-26 21:28:23 +0530

    Added Stripe apis

--- src/lib/actions/transaction.action.js
+++ src/lib/actions/transaction.action.js
@@ -1,14 +1,14 @@
 "use server";
 
-import { redirect } from 'next/navigation'
-import Stripe from "stripe";
-import { handleError } from '../utils';
-import { connectToDatabase } from '../database/mongoose';
-import Transaction from '../database/models/transaction.model';
-import { updateCredits } from './user.actions';
+const { redirect } = require('next/navigation');
+const Stripe = require("stripe");
+const { handleError } = require('../utils');
+const { connectToDatabase } = require('../database/mongoose');
+const Transaction = require('../database/models/transaction.model');
+const { updateCredits } = require('./user.actions');
 
-export async function checkoutCredits(transaction: CheckoutTransactionParams) {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+async function checkoutCredits(transaction) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
   const amount = Number(transaction.amount) * 100;
 
@@ -33,24 +33,30 @@ export async function checkoutCredits(transaction: CheckoutTransactionParams) {
     mode: 'payment',
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
-  })
+  });
 
-  redirect(session.url!)
+  redirect(session.url);
 }
 
-export async function createTransaction(transaction: CreateTransactionParams) {
+async function createTransaction(transaction) {
   try {
     await connectToDatabase();
 
     // Create a new transaction with a buyerId
     const newTransaction = await Transaction.create({
-      ...transaction, buyer: transaction.buyerId
-    })
+      ...transaction,
+      buyer: transaction.buyerId
+    });
 
     await updateCredits(transaction.buyerId, transaction.credits);
 
     return JSON.parse(JSON.stringify(newTransaction));
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 }
+
+module.exports = {
+  checkoutCredits,
+  createTransaction
+};
